a63ec2ff4d77c7c6fcaac1e6888b302de4dfdbb1
marbsouza <mariana.barbosa@g.globo>
2023-10-26 11:57:14 -0300

    feat: implemented Order entity and made associations

--- project1/project1/src/main/java/com/project/project1/entities/User.java
+++ project1/project1/src/main/java/com/project/project1/entities/User.java
@@ -1,12 +1,15 @@
 package com.project.project1.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +26,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	//Association 1 User can have many Orders, need to be instantiate because is a collection:
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {}
 	public User(Long id, String name, String email, String phone, String password) {
@@ -73,6 +79,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	// Collections just have getters
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -90,6 +100,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
 
 
